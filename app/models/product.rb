3422800ab9709a845351910f842e63d1cc886ebc
dhruv-mehta <dhm@narola.email>
2016-07-11 15:31:56 +0530

    code optimization for the validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,6 @@
 class Product < ActiveRecord::Base
-
-def method_name
-	
-end
-validates_presence_of :title,  :message => "can't be blank"
-validates_presence_of :description,  :message => "can't be blank"
-validates_presence_of :image_url,  :message => "can't be blank"
-validates_presence_of :price, :message => "can't be blank"
-validates_uniqueness_of :title , :message => "must be unique"
-validates_numericality_of :price, :greater_than => 0.01,  :message => "must be grater then 0.01"
-validates :image_url, format: {with: /\.(png|jpg)\Z/i}
+validates :title, :uniqueness => {:message =>  " already exsits"} , presence: true
+validates :description , presence: true
+validates :image_url , presence: true , format: {with: /\.(png|jpg)\Z/i}
+validates :price, :numericality => { :greater_than => 0.01  } 
 end
