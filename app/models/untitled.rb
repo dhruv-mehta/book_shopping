ff438ccd4b7dc31005c0ccb5387aa9103f172f9e
dhruv-mehta <dhm@narola.email>
2016-07-18 12:23:47 +0530

    remove unnecrssary model

--- app/models/untitled.rb
+++
@@ -1,6 +0,0 @@
-@line_item = @cart.line_items.find_by(:cart_id => params[:cart_id])
-   
-   
-       @line_item.update_attributes(:order_id => (@order.id))
-   puts "************#{@line_item.inspect}"
-    
