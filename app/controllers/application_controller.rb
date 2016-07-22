4a3eea4e7604581f690403e3efa7433c5f345a98
dhruv-mehta <dhm@narola.email>
2016-07-22 18:00:46 +0530

    internationalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,14 +9,10 @@ class ApplicationController < ActionController::Base
 
 	def set_i18n_locale_from_params 
 
-		puts "this method excute every time"
+	
 
 			if params[:locale]
-
-				session[:locale]="param"
-		puts session[:locale]
-				session[:locale]=params[:locale]
-				if I18n.available_locales.include?(params[:locale].to_sym) 
+			if I18n.available_locales.include?(params[:locale].to_sym) 
 						I18n.locale = params[:locale]
 				else
 					flash.now[:notice] ="#{params[:locale]} translation not available"
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,4 @@
-class StoreController < BaseController
+class StoreController < ApplicationController
 
  include CreateCart
 
